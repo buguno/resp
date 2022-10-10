332a4a1c4a4f4e74eaccf630a891d827d23c78d9
buguno <brunnodesouzabezerra@gmail.com>
2022-10-09 22:45:52 -0300

    refactor: adding new routes

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"resp-api/pkg/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+
+	routes.EspRoute(app)
+	routes.NotFoundRoute(app) // Register route for 404 Error.
 
 	app.Get("/", func(context *fiber.Ctx) error {
 		return context.JSON(&fiber.Map{"data": "Hello, World 👋!"})
